480fe32a519ec91b1d237ffc8b991a1bcbdf16f3
jacobwojoski <jacobwojoski@gmail.com>
2026-01-15 22:28:39 -0500

    Get working

--- src/main/java/org/wojo/wojosHammers/ExampleCommand.java
+++ src/main/java/org/wojo/wojosHammers/ExampleCommand.java
@@ -1,4 +1,4 @@
-package org.example.plugin;
+package org.wojo.wojosHammers;
 
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.Message;
--- src/main/java/org/wojo/wojosHammers/ExamplePlugin.java
+++ src/main/java/org/wojo/wojosHammers/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package org.example.plugin;
+package org.wojo.wojosHammers;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
