42cc1e25a6cf8ca2d64b007606df1d439a947450
jacobwojoski <jacobwojoski@gmail.com>
2026-01-19 23:10:06 -0500

    Get Test Interactions Working

--- src/main/java/org/wojo/wojosHammers/Interactions/MiningHammerInteractionPrimary.java
+++ src/main/java/org/wojo/wojosHammers/Interactions/MiningHammerInteractionPrimary.java
@@ -1,13 +1,24 @@
 package org.wojo.wojosHammers.Interactions;
 
 import com.hypixel.hytale.codec.builder.BuilderCodec;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.protocol.InteractionState;
 import com.hypixel.hytale.protocol.InteractionType;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 public class MiningHammerInteractionPrimary extends SimpleInstantInteraction {
+    // TODO: For Testing only
+    public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
     // CODEX: Needed to link interaction with item
     public static final BuilderCodec<MiningHammerInteractionPrimary> CODEC = BuilderCodec.builder(
@@ -17,6 +28,18 @@ public class MiningHammerInteractionPrimary extends SimpleInstantInteraction {
     @Override
     protected void firstRun(@NonNullDecl InteractionType interactionType, @NonNullDecl InteractionContext interactionContext, @NonNullDecl CooldownHandler cooldownHandler) {
         // Custom Behavior when item is used
+        CommandBuffer<EntityStore> commandBuffer = interactionContext.getCommandBuffer();
+        if (commandBuffer == null) {
+            interactionContext.getState().state = InteractionState.Failed;
+            LOGGER.atInfo().log("CommandBuffer is null");
+            return;
+        }
+
+        World world = commandBuffer.getExternalData().getWorld();
+        Store<EntityStore> store = commandBuffer.getExternalData().getStore();
+        Ref<EntityStore> ref = interactionContext.getEntity();
+        Player player = commandBuffer.getComponent(ref, Player.getComponentType());
+        player.sendMessage(Message.raw("You have used the custom item Primary Interaction"));
     }
 
 }
